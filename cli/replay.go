0f664f387971b5da177f860387287a9f3ef306aa
Saad7890-web <omysaadislam@gmail.com>
2026-03-17 19:44:09 +0600

    Replay engine feature added

--- cli/replay.go
+++ cli/replay.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 
+	"github.com/Saad7890-web/twinflow/internal/replay"
 	"github.com/spf13/cobra"
 )
 
@@ -12,14 +13,19 @@ var replayTarget string
 var replayCmd = &cobra.Command{
 	Use:   "replay",
 	Short: "Replay captured traffic",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Replaying traffic from:", captureDir)
-		fmt.Println("Target service:", replayTarget)
-	},
+Run: func(cmd *cobra.Command, args []string) {
+
+	engine := replay.NewEngine(replayTarget)
+
+	err := engine.Replay(captureDir)
+	if err != nil {
+		fmt.Println("Replay failed:", err)
+	}
+},
 }
 
 func init() {
-	replayCmd.Flags().StringVar(&captureDir, "capture", "./captures", "capture directory")
+	replayCmd.Flags().StringVar(&captureDir, "capture", "captures/traffic.log", "capture file")
 	replayCmd.Flags().StringVar(&replayTarget, "target", "", "target service URL")
 
 	replayCmd.MarkFlagRequired("target")
